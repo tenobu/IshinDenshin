0336302c677b2d549a6507c0e7eddd27b143cd0c
tenobu <bizan09@bizankomumakku09-no-MacBook-Air.local>
2014-09-18 13:50:12 +0900

    Initial Commit

---
+++ IshinDenshin/NTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NTAppDelegate.h
+//  IshinDenshin
+//
+//  Created by ビザンコムマック０９ on 2014/09/18.
+//  Copyright (c) 2014年 ビザンコムマック０９. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IshinDenshin/NTViewController.h
@@ -0,0 +1,13 @@
+//
+//  NTViewController.h
+//  IshinDenshin
+//
+//  Created by ビザンコムマック０９ on 2014/09/18.
+//  Copyright (c) 2014年 ビザンコムマック０９. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTViewController : UIViewController
+
+@end
